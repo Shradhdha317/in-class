32811306963109163ca7129a0a09cfd902ea0619
patels183 <74590893+patels183@users.noreply.github.com>
2021-03-03 16:15:56 -0500

    Started working with js

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app
+    .get('/', (req, res) => {
+    res.send('Hello World!')
+    })
+    .get('/purim', (req, res) => {
+        res.send('I am learning web server programming!');
+    })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
