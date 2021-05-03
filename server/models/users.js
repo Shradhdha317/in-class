defaa871c7e26fe49d297f78997cd6ce0aa9ea74
patels183 <74590893+patels183@users.noreply.github.com>
2021-05-03 00:41:08 -0400

    JSON web tokens and personalization

--- server/models/users.js
+++ server/models/users.js
@@ -2,6 +2,10 @@
 
 */
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SALT_ROUNDS = process.env.SALT_ROUNDS;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const list = [
     { 
@@ -9,7 +13,9 @@ const list = [
         lastName: 'Plotkin',
         handle: '@JewPaltz',
         pic: 'https://bulma.io/images/placeholders/96x96.png',
-        password: 'Me',
+        password: '$2b$08$ovDdePT2UjP9nkMaOhpFgOQEsBclWpB9RfS2p5XZwq.vDIzwNw5ke',
+        isAdmin: true,
+        following: [ { handle: '@vp', isApproved: true }, { handle: '@johnsmith', isApproved: true }, ],
     },
     { 
         firstName: 'Kamala',
@@ -17,6 +23,8 @@ const list = [
         handle: '@vp',
         pic: 'https://bulma.io/images/placeholders/96x96.png',
         password: 'Her',
+        isAdmin: true,
+        following: [ { handle: '@johnsmith', isApproved: true }, ],
     },
     { 
         firstName: 'John',
@@ -24,6 +32,8 @@ const list = [
         handle: '@johnsmith',
         pic: 'https://bulma.io/images/placeholders/96x96.png',
         password: 'BeepBop',
+        isAdmin: true,
+        following: [ { handle: '@vp', isApproved: true }, ],
     },
 
 ];
@@ -40,7 +50,7 @@ module.exports.Add = (user)=> {
 }
 module.exports.Register = async (user)=> {
 
-    const hash = await bcrypt.hash(user.password, 8);
+    const hash = await bcrypt.hash(user.password, +SALT_ROUNDS);
 
     user.password = hash;
 
@@ -75,10 +85,29 @@ module.exports.Delete = (user_id)=> {
     return user;
 }
 
-module.exports.Login = (handle, password) =>{
+module.exports.Login = async (handle, password) =>{
     console.log({ handle, password})
-    const user = list.find(x=> x.handle == handle && x.password == password);
-    if(!user) throw { code: 401, msg: "Wrong Username or Password" };
+    const user = list.find(x=> x.handle == handle);
+    if(!user) throw { code: 401, msg: "Sorry there is no user with that handle" };
 
-    return user;
+    if( ! await bcrypt.compare(password, user.password) ){
+        throw { code: 401, msg: "Wrong Password" };
+    }
+
+    const data = { ...user, password: undefined };
+
+    const token = jwt.sign(data, JWT_SECRET)
+
+    return { user: data, token };
+}
+
+module.exports.FromJWT = async (token) =>{
+    try {
+        const user = jwt.verify(token, JWT_SECRET);
+        return user;       
+    } catch (error) {
+        console.log({error});
+        return null;
+    }
+
 }
