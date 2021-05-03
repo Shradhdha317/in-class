4b0e4db411dcaaccedac87b11b13a54b7539090f
patels183 <74590893+patels183@users.noreply.github.com>
2021-05-02 22:26:45 -0400

    created our first server side controller and added posts controller

--- server/index.js
+++ server/index.js
@@ -3,17 +3,21 @@
 
 const path = require('path');
 const express = require('express');
+
+const usersCtrl = require('./controllers/users');
+const postsCtrl = require('./controllers/posts');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
+    .use(express.json())
     .use(express.static('./docs'))
-    .get('/', (req, res) => {
-        res.send('Hello World!')
-    })
-    .get('/purim', (req, res) => {
-        res.send('Lechaim, to life!');
-    })
+
+    .use('/users', usersCtrl)
+    .use('/posts', postsCtrl)
+
+
     // All the way at the end of the pipeline. Return instead of not found
     .get('*', (req, res) => {
         res.sendFile( path.join(__dirname, '../docs/index.html' ) );
