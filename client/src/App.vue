c7aeddbc314fcb226aeaf304fe1c9071fb474922
patels183 <74590893+patels183@users.noreply.github.com>
2021-05-04 10:32:21 -0400

    added fb messenger and facebook login

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,11 @@
       <router-view/>
     </div>
 
-
+      <div id="fb-root"></div>
+      <div class="fb-customerchat"
+        attribution="biz_inbox"
+        page_id="267612484194">
+      </div>
   </div>
 </template>
 
@@ -17,8 +21,31 @@ export default {
     MyNav
   }
 }
+        /*global FB*/
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '5392384337469403',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v10.0'
+          });
+            
+          FB.AppEvents.logPageView();   
+            
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
 </script>
+
 
 <style lang="scss">
 
 </style>
+
--- client/src/models/Users.js
+++ client/src/models/Users.js
@@ -1,3 +1,4 @@
+
 import { api } from "./myFetch";
 
 export function GetAllUsers(){
